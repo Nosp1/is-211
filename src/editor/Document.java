3ae7c6f71e49aa9bb9f85863219876d15026f71e
Trym Erlend Staurheim <trymes13%student.uia.no>
2020-01-21 14:39:54 +0100

    så langt

---
+++ src/editor/Document.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package editor;
+
+import editor.display.CharacterDisplay;
+
+/**
+ * This class represents the document being edited. Using a 2d array to hold the
+ * document content is probably not a very good choice. Fixing this class is the
+ * main focus of the exercise. In addition to designing a better data model, you
+ * must add methods to do at least basic editing: write and delete text, and
+ * moving the cursor
+ *
+ * @author evenal
+ */
+public class Document {
+
+    private CharacterDisplay display;
+    private int cursorRow;
+    private int cursorCol;
+    private char[][] data;
+
+    public Document(CharacterDisplay display) {
+        this.display = display;
+        data = new char[CharacterDisplay.HEIGHT][CharacterDisplay.WIDTH];
+        cursorCol = cursorRow = 0;
+    }
+
+    public void insertChar(char c) {
+        data[cursorRow][cursorCol] = c;
+        System.out.println("value of c " + c);
+        if (c == ' ') {
+            deleteChar(c);
+        } else {
+            System.out.println("value of c " + c);
+            display.displayChar(c, cursorRow, cursorCol);
+            cursorCol++;
+            if (cursorCol >= CharacterDisplay.WIDTH) {
+                cursorCol = 0;
+                cursorRow++;
+            }
+        }
+    }
+
+    public void deleteChar(char c) {
+        data[cursorRow][cursorCol] = c;
+        display.displayChar(' ',cursorRow,cursorCol);
+        cursorCol--;
+        if (cursorCol >= CharacterDisplay.WIDTH) {
+            cursorCol = 0;
+            cursorRow++;
+        }
+    }
+}
